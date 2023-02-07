4e18dc1d0e211170b0cef2c26101497a6a87f8c0
TamaraKoval <kovalpsy@mail.ru>
2023-02-07 14:25:19 +0300

    Comparator commit

--- src/person/Person.java
+++ src/person/Person.java
@@ -1,4 +1,18 @@
 package person;
 
 public class Person {
+    private String name;
+    private String surname;
+    private int age;
+
+    public Person(String name, String surname, int age) {
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return name + " " + surname + ": " + age;
+    }
 }
