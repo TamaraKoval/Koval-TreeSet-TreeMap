b327cf3ee9a24cfc4a89bf7a14270374bc14aaa0
TamaraKoval <kovalpsy@mail.ru>
2023-02-08 19:22:10 +0300

    lambda v2

--- src/Main.java
+++ src/Main.java
@@ -14,9 +14,7 @@ public class Main {
         noblePeople.add(new Person("Виктория", "Алиса Елена Луиза Беатриса Гессен-Дармштадская", 46));
         noblePeople.add(new Person("Фридерика", "Луиза Шарлотта Вильгельмина Прусская", 62));
 
-        Comparator<Person> comparator = new Comparator<Person>() {
-            @Override
-            public int compare(Person o1, Person o2) {
+        Comparator<Person> comparator = (Person o1, Person o2) -> {
                 int o1SurnameLength = o1.getSurname().split("-| ").length;
                 int o1SignificantLength = Math.min(o1SurnameLength, 5);
 
@@ -30,7 +28,6 @@ public class Main {
                 } else {
                     return Integer.compare(o2.getAge(), o1.getAge());
                 }
-            }
         };
 
         Collections.sort(noblePeople, comparator); // сортируем список, значимое количество - 5
